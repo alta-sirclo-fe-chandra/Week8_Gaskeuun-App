47036e5c2ab70b6ca62feb08992a466aba482881
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-05 11:41:33 +0700

    feat: create event/create page

--- pages/event/create.tsx
+++ pages/event/create.tsx
@@ -1,31 +1,65 @@
 import { useRouter } from "next/router";
-import { FormEvent } from "react";
-import { LoadingButton } from "@mui/lab";
+import { FormEvent, useState, ChangeEvent, useRef } from "react";
+import { styled } from "@mui/material/styles";
+import { DateTimePicker, LoadingButton, LocalizationProvider } from "@mui/lab";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import {
   Box,
   createTheme,
   FormLabel,
   Grid,
-  InputBase,
+  MenuItem,
+  TextField,
   Typography,
   useMediaQuery,
 } from "@mui/material";
-import { useRef } from "react";
-import Layout from "../../layouts/index";
-import { button, inputForm, labelForm } from "../../styles/formStyle";
 
+import Layout from "../../layouts/index";
+import { button, labelForm } from "../../styles/formStyle";
 import { navy } from "../../styles/colorStyle";
 import { backToHome } from "../../styles/profileStyle";
 
 const theme = createTheme();
 
+const categories = [
+  "Sport",
+  "Game",
+  "Art",
+  "Technology",
+  "Music",
+  "Education",
+  "Others",
+];
+
+const CustomTextField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "#2A6AC8",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#2A6AC8",
+  },
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#2A6AC8",
+    },
+    "&:hover fieldset": {
+      borderColor: "#2A6AC8",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#2A6AC8",
+    },
+  },
+});
+
 const Create = () => {
   const imageRef = useRef<HTMLInputElement>();
   const titleRef = useRef<HTMLInputElement>();
   const hostedByRef = useRef<HTMLInputElement>();
-  const dateTimeRef = useRef<HTMLInputElement>();
   const locationRef = useRef<HTMLInputElement>();
   const descriptionRef = useRef<HTMLInputElement>();
+
+  const [category, setCategory] = useState<string>("Sport");
+  const [value, setValue] = useState<Date | null>(new Date());
 
   const router = useRouter();
 
@@ -37,8 +71,18 @@ const Create = () => {
     router.push("/");
   };
 
+  const handleCategoryChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCategory(e.target.value);
+  };
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log("value =>", value, "==", typeof value, value?.getFullYear());
+    console.log("image =>", imageRef.current?.value!);
+    console.log("title =>", titleRef.current?.value!);
+    console.log("hostedBy =>", hostedByRef.current?.value!);
+    console.log("location =>", locationRef.current?.value!);
+    console.log("description =>", descriptionRef.current?.value!);
   };
 
   return (
@@ -50,100 +94,127 @@ const Create = () => {
           </Typography>
 
           <Box component="form" onSubmit={handleSubmit} pb={5}>
+            {/* untuk image */}
             <Grid container>
-              <Grid item xs={12}>
-                <FormLabel sx={labelForm} required={true}>
-                  Image
-                </FormLabel>
-                <InputBase
-                  required
-                  fullWidth
-                  id="image"
-                  name="image"
-                  sx={inputForm}
-                  placeholder="https://source.unsplash.com/random"
-                  inputRef={imageRef}
-                />
+              <Grid item xs={12} mb={2}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <FormLabel sx={{ ...labelForm, mb: 1 }} required={true}>
+                    Image
+                  </FormLabel>
+                  <CustomTextField
+                    id="image"
+                    name="image"
+                    placeholder="https://source.unsplash.com/random"
+                    inputRef={imageRef}
+                  />
+                </Box>
               </Grid>
             </Grid>
 
-            <Grid container spacing={isMobile ? 0 : 3}>
-              <Grid item xs={12} md={6}>
-                <FormLabel sx={labelForm} required={true}>
-                  Title
-                </FormLabel>
-                <InputBase
-                  required
-                  fullWidth
-                  id="title"
-                  name="title"
-                  sx={inputForm}
-                  placeholder="Baking Brownies"
-                  inputRef={titleRef}
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <FormLabel sx={labelForm} required={true}>
-                  Hosted By
-                </FormLabel>
-                <InputBase
-                  required
-                  fullWidth
-                  id="hostedBy"
-                  name="hostedBy"
-                  sx={inputForm}
-                  placeholder="Brownies Lover"
-                  inputRef={hostedByRef}
-                />
-              </Grid>
-            </Grid>
-
-            <Grid container spacing={isMobile ? 0 : 3}>
-              <Grid item xs={12} md={6}>
-                <FormLabel sx={labelForm} required={true}>
-                  Date/Time
-                </FormLabel>
-                <InputBase
-                  required
-                  fullWidth
-                  id="dateTime"
-                  name="dateTime"
-                  sx={inputForm}
-                  inputRef={dateTimeRef}
-                />
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <FormLabel sx={labelForm} required={true}>
-                  Location
-                </FormLabel>
-                <InputBase
-                  required
-                  fullWidth
-                  id="location"
-                  name="location"
-                  sx={inputForm}
-                  placeholder="Menara Bidakara, Jakarta"
-                  inputRef={locationRef}
-                />
-              </Grid>
-            </Grid>
-
+            {/* untuk title */}
             <Grid container>
-              <Grid item xs={12}>
-                <FormLabel sx={labelForm} required={true}>
-                  Description
-                </FormLabel>
-                <InputBase
-                  required
-                  fullWidth
-                  id="description"
-                  name="description"
-                  sx={{ ...inputForm, height: "10rem" }}
-                  placeholder="Tell people about your event"
-                  inputRef={descriptionRef}
-                  multiline={true}
-                  rows={6}
-                />
+              <Grid item xs={12} mb={2}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <FormLabel sx={{ ...labelForm, mb: 1 }} required={true}>
+                    Title
+                  </FormLabel>
+                  <CustomTextField
+                    id="title"
+                    name="title"
+                    placeholder="Baking Brownies"
+                    inputRef={titleRef}
+                  />
+                </Box>
+              </Grid>
+            </Grid>
+
+            {/* utk category dan hostedby */}
+            <Grid container spacing={isMobile ? 0 : 4}>
+              <Grid item xs={12} md={6} mb={2}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <FormLabel sx={{ ...labelForm, mb: 1 }} required={true}>
+                    Category
+                  </FormLabel>
+                  <CustomTextField
+                    id="outlined-select-currency"
+                    select
+                    value={category}
+                    onChange={handleCategoryChange}
+                  >
+                    {categories.map((category, idx) => (
+                      <MenuItem key={idx} value={category}>
+                        {category}
+                      </MenuItem>
+                    ))}
+                  </CustomTextField>
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={6} mb={2}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <FormLabel sx={{ ...labelForm, mb: 1 }} required={true}>
+                    Hosted By
+                  </FormLabel>
+                  <CustomTextField
+                    id="hostedBy"
+                    name="hostedBy"
+                    placeholder="Brownies Lover"
+                    inputRef={hostedByRef}
+                  />
+                </Box>
+              </Grid>
+            </Grid>
+
+            {/* untuk date/time dan location */}
+            <Grid container spacing={isMobile ? 0 : 4}>
+              <Grid item xs={12} md={6} mb={2}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <FormLabel sx={{ ...labelForm, mb: 1 }} required={true}>
+                    Date/Time
+                  </FormLabel>
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DateTimePicker
+                      renderInput={(props) => (
+                        <CustomTextField {...props} fullWidth />
+                      )}
+                      value={value}
+                      onChange={(newValue) => {
+                        setValue(newValue);
+                      }}
+                    />
+                  </LocalizationProvider>
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={6} mb={2}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <FormLabel sx={{ ...labelForm, mb: 1 }} required={true}>
+                    Location
+                  </FormLabel>
+                  <CustomTextField
+                    id="location"
+                    name="location"
+                    placeholder="Menara Bidakara, Jakarta"
+                    inputRef={locationRef}
+                  />
+                </Box>
+              </Grid>
+            </Grid>
+
+            {/* untuk description */}
+            <Grid container>
+              <Grid item xs={12} mb={2}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <FormLabel sx={{ ...labelForm, mb: 1 }} required={true}>
+                    Description
+                  </FormLabel>
+                  <CustomTextField
+                    id="description"
+                    name="description"
+                    placeholder="Tell people about your event"
+                    multiline
+                    rows={5}
+                    inputRef={descriptionRef}
+                  />
+                </Box>
               </Grid>
             </Grid>
 
