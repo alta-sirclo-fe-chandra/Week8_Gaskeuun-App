e80221901e0184ae9e09ee1cb40a38e16e4fcba8
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-06 23:14:09 +0700

    feat: get user data in profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,23 +1,52 @@
 import { useRouter } from "next/router";
-import { useRef, FormEvent } from "react";
+import { useRef, FormEvent, useState } from "react";
 import { LoadingButton } from "@mui/lab";
 import { Avatar, Box, FormLabel, Grid, InputBase } from "@mui/material";
 
 import HeadPage from "../components/head";
 import Layout from "../layouts/index";
-import { inputForm, labelForm, button } from "../styles/formStyle";
+import {
+  inputForm,
+  labelForm,
+  button,
+  CustomTextField,
+} from "../styles/formStyle";
 import {
   leftContent,
   pageContainer,
   profilePicture,
   rightContent,
 } from "../styles/profileStyle";
+import {
+  gridItemMargin,
+  itemContainer,
+  label,
+} from "../styles/createUpdateStyle";
+import { useQuery } from "@apollo/client";
+import { GET_USER } from "../libs/queries";
+import { User } from "../types/User";
 
 const Profile = () => {
   const nameRef = useRef<HTMLInputElement>();
   const emailRef = useRef<HTMLInputElement>();
   const passwordRef = useRef<HTMLInputElement>();
-  const pictureRef = useRef<HTMLInputElement>();
+  const profilePictureRef = useRef<HTMLInputElement>();
+
+  const router = useRouter();
+
+  let userId = 0;
+
+  if (typeof window !== "undefined") {
+    userId = Number(localStorage.getItem("userId"));
+  }
+
+  const { data } = useQuery(GET_USER, {
+    variables: { userId },
+  });
+
+  if (data) {
+    console.log("ini data =>", data);
+  }
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -26,97 +55,109 @@ const Profile = () => {
   return (
     <>
       <HeadPage />
-      <Layout>
-        <Box sx={pageContainer}>
-          <Grid container maxWidth="lg">
-            <Grid item xs={12} md={6} sx={leftContent}>
-              <Avatar
-                alt="User"
-                src="https://source.unsplash.com/random"
-                sx={profilePicture}
-              ></Avatar>
+      {data && (
+        <Layout>
+          <Box sx={pageContainer}>
+            <Grid container maxWidth="lg">
+              <Grid item xs={12} md={6} sx={leftContent}>
+                <Avatar
+                  alt="User"
+                  src="https://source.unsplash.com/random"
+                  sx={profilePicture}
+                ></Avatar>
+              </Grid>
+
+              <Grid item xs={12} md={6} sx={rightContent}>
+                <Box component="form" onSubmit={handleSubmit}>
+                  <Grid container>
+                    <Grid item xs={12} sx={gridItemMargin}>
+                      <Box sx={itemContainer}>
+                        <FormLabel sx={label} required={true}>
+                          Name
+                        </FormLabel>
+                        <CustomTextField
+                          required
+                          id="name"
+                          name="name"
+                          defaultValue={data.getUser.name}
+                          inputRef={nameRef}
+                        />
+                      </Box>
+                    </Grid>
+                  </Grid>
+
+                  <Grid container>
+                    <Grid item xs={12} sx={gridItemMargin}>
+                      <Box sx={itemContainer}>
+                        <FormLabel sx={label} required={true}>
+                          Email
+                        </FormLabel>
+                        <CustomTextField
+                          required
+                          id="email"
+                          name="email"
+                          defaultValue={data.getUser.email}
+                          inputRef={emailRef}
+                        />
+                      </Box>
+                    </Grid>
+                  </Grid>
+
+                  <Grid container>
+                    <Grid item xs={12} sx={gridItemMargin}>
+                      <Box sx={itemContainer}>
+                        <FormLabel sx={label} required={true}>
+                          Password
+                        </FormLabel>
+                        <CustomTextField
+                          required
+                          type="password"
+                          id="password"
+                          name="password"
+                          inputRef={passwordRef}
+                        />
+                      </Box>
+                    </Grid>
+                  </Grid>
+
+                  <Grid container>
+                    <Grid item xs={12} sx={gridItemMargin}>
+                      <Box sx={itemContainer}>
+                        <FormLabel sx={label} required={true}>
+                          Profile Picture
+                        </FormLabel>
+                        <CustomTextField
+                          required
+                          id="profile-picture"
+                          name="profile-picture"
+                          defaultValue={
+                            data.getUser.imageUrl
+                              ? data.getUser.imageUrl
+                              : "set yout url"
+                          }
+                          inputRef={profilePictureRef}
+                        />
+                      </Box>
+                    </Grid>
+                  </Grid>
+
+                  <LoadingButton
+                    // loading={isLoading}
+                    loadingIndicator="Loading..."
+                    variant="contained"
+                    type="submit"
+                    size="large"
+                    sx={button}
+                    fullWidth
+                  >
+                    Edit Profile
+                  </LoadingButton>
+                </Box>
+              </Grid>
             </Grid>
-
-            <Grid item xs={12} md={6} sx={rightContent}>
-              <Box component="form" onSubmit={handleSubmit}>
-                <FormLabel sx={labelForm} required={true}>
-                  Name
-                </FormLabel>
-                <InputBase
-                  margin="dense"
-                  required
-                  fullWidth
-                  id="name"
-                  name="name"
-                  autoComplete="name"
-                  sx={inputForm}
-                  defaultValue="John Doe"
-                  inputRef={nameRef}
-                />
-
-                <FormLabel sx={labelForm} required={true}>
-                  Email
-                </FormLabel>
-                <InputBase
-                  margin="dense"
-                  required
-                  fullWidth
-                  id="email"
-                  name="email"
-                  autoComplete="email"
-                  sx={inputForm}
-                  defaultValue="example@domain.com"
-                  inputRef={emailRef}
-                />
-
-                <FormLabel sx={labelForm} required={true}>
-                  Password
-                </FormLabel>
-                <InputBase
-                  margin="dense"
-                  required
-                  fullWidth
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="password"
-                  sx={inputForm}
-                  autoFocus
-                  inputRef={passwordRef}
-                />
-
-                <FormLabel sx={labelForm} required={true}>
-                  Profile Picture
-                </FormLabel>
-                <InputBase
-                  margin="dense"
-                  required
-                  fullWidth
-                  id="picture"
-                  name="picture"
-                  type="picture"
-                  autoComplete="picture"
-                  sx={inputForm}
-                  defaultValue="https://source.unsplash.com/random"
-                  inputRef={pictureRef}
-                />
-
-                <LoadingButton
-                  // loading={isLoading}
-                  loadingIndicator="Loading..."
-                  variant="contained"
-                  type="submit"
-                  size="large"
-                  sx={button}
-                  fullWidth
-                >
-                  Edit Profile
-                </LoadingButton>
-              </Box>
-            </Grid>
-          </Grid>
-        </Box>
-      </Layout>
+          </Box>
+        </Layout>
+      )}
     </>
   );
 };
