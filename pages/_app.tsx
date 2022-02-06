424dc602109fe38d229d681afa3f320c5dd7602b
Miratunnisa Umarputri <ratuuaw@gmail.com>
2022-02-06 17:42:35 +0700

    feat: create user context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,20 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../libs/apollo";
 import AuthContext from "../store/AuthContext";
 import { useState } from "react";
+import UserContext from "../store/UserContext";
+import { User } from "../types/User";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState({} as User);
 
   return (
     <AuthContext.Provider value={{ isAuth, setIsAuth }}>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <UserContext.Provider value={{ user, setUser }}>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </UserContext.Provider>
     </AuthContext.Provider>
   );
 }
